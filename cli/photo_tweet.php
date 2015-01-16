e5a281c7456a09f3e34d0d98e3f27caf17530b68
themattharris <mharris@twitter.com>
2015-01-15 20:16:10 -0800

    update the photo tweet example to use the media upload api

--- cli/photo_tweet.php
+++ cli/photo_tweet.php
@@ -13,15 +13,47 @@ if (class_exists('CurlFile', false)) {
   $media = "@{$image};type=image/jpeg;filename={$name}";
 }
 
+// ref: https://dev.twitter.com/rest/public/uploading-media
+// first we post to the media upload endpoint
+// then we use the returned information to post to the tweet endpoint
 
 $code = $tmhOAuth->user_request(array(
   'method' => 'POST',
-  'url' => $tmhOAuth->url('1.1/statuses/update_with_media'),
+  'url'    => 'https://upload.twitter.com/1.1/media/upload.json',
   'params' => array(
-    'media[]'  => $media,
-    'status'   => $status,
+    'media' => $media,
   ),
   'multipart' => true,
 ));
+
+$tmhOAuth->render_response();
+
+// response looks like this:
+/*
+{
+  "media_id": 553656900508606464,
+  "media_id_string": "553656900508606464",
+  "size": 998865,
+  "image": {
+    "w": 2234,
+    "h": 1873,
+    "image_type": "image/jpeg"
+  }
+}
+*/
+
+if ($code == 200) {
+  $data = json_decode($tmhOAuth->response['response']);
+  $media_id = $data->media_id_string;
+
+  $code = $tmhOAuth->user_request(array(
+    'method' => 'POST',
+    'url'    => $tmhOAuth->url('1.1/statuses/update'),
+    'params' => array(
+      'media_ids' => $media_id,
+      'status'    => $status,
+    )
+  ));
+}
 
 $tmhOAuth->render_response();
